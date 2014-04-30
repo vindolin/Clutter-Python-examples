e9673c577d1009d3c92416482cf1693f6a605725
vindolin <vindolin@gmail.com>
2014-04-30 12:22:48 +0200

    oops, forgot to add a file

--- cairo_content.py
+++ cairo_content.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 from gi.repository import Clutter, GLib
 import cairo
+import math
 
 
-def rounded_rect(ctx, x, y, w, h, r):
-    ctx.move_to(x + r, y)
-    ctx.line_to(x + w - r, y)
-    ctx.curve_to(x + w, y, x + w, y, x + w, y + r)
-    ctx.line_to(x + w, y + h - r)
-    ctx.curve_to(x + w, y + h, x + w, y + h, x + w - r, y + h)
-    ctx.line_to(x + r, y + h)
-    ctx.curve_to(x, y + h, x, y + h, x, y + h - r)
-    ctx.line_to(x, y + r)
-    ctx.curve_to(x, y, x, y, x + r, y)
+def rounded_rect(ctx, x, y, width, height, radius, line_width):
+    # FIXME the line with have to be included into the calculation
+    # or else the lines overlap with small radii
+    line_width
+    degrees = math.pi / 180.0
+
+    ctx.new_sub_path()
+    ctx.arc(x + width - radius, y + radius, radius, -90 * degrees, 0 * degrees)
+    ctx.arc(x + width - radius, y + height - radius, radius, 0 * degrees, 90 * degrees)
+    ctx.arc(x + radius, y + height - radius, radius, 90 * degrees, 180 * degrees)
+    ctx.arc(x + radius, y + radius, radius, 180 * degrees, 270 * degrees)
+    ctx.close_path()
 
 color = lambda string: Clutter.color_from_string(string)[1]  # shortcut
 
@@ -30,14 +33,18 @@ class CairoActor(Clutter.Actor):
         self.canvas = Clutter.Canvas()
         self.set_content(self.canvas)
         self.canvas.connect('draw', self.draw)
+        self.line_width = 5
+        self.border_radius = 50
+        self.refresh_millis = 10
+        self.fill_color = 0.39, 0.58, 0.93  # crimson
+        self.stroke_color = 0.8, 0.27, 0  # orange red
 
         self.idle_resize_id = 0
         self.connect('notify::allocation', self.on_allocation)
-        self.canvas.invalidate()
 
     def on_allocation(self, *_):
         if self.idle_resize_id == 0:
-            self.idle_resize_id = Clutter.threads_add_timeout(GLib.PRIORITY_DEFAULT, 100, self.idle_resize)
+            self.idle_resize_id = Clutter.threads_add_timeout(GLib.PRIORITY_DEFAULT, self.refresh_millis, self.idle_resize)
 
     def idle_resize(self):
         self.canvas.invalidate()
@@ -45,22 +52,29 @@ class CairoActor(Clutter.Actor):
         self.idle_resize_id = 0
 
     def draw(self, canvas, ctx, width, height):
+        border_radius = self.border_radius
+        if width <= border_radius * 2 or height <= border_radius * 2:
+            border_radius = min((width, height)) / 2
+
+        # clear the previous frame
+        ctx.set_operator(cairo.OPERATOR_CLEAR)
+        ctx.paint()
+
         ctx.set_operator(cairo.OPERATOR_OVER)
         rounded_rect(
             ctx,
-            0,
-            0,
-            width,
-            height,
-            100
+            0 + self.line_width,
+            0 + self.line_width,
+            width - self.line_width * 2,
+            height - self.line_width * 2,
+            border_radius,
+            self.line_width
         )
-        ctx.set_source_rgb(0.86, 0.08, 0.24)  # crimson
-        ctx.fill_preserve()
-        ctx.set_line_width(5)
-        ctx.set_source_rgb(0.8, 0.27, 0)  # orange red
+        ctx.set_source_rgb(*self.fill_color)
+        ctx.fill_preserve()  # fill but keep the rectangle
+        ctx.set_line_width(self.line_width)
+        ctx.set_source_rgb(*self.stroke_color)
         ctx.stroke()
-
-        return True
 
 
 def stage_key(element, event):
